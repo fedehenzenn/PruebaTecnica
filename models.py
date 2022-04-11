c1e4ab4ff9efbba034d52fa6e3f9ed8ddeab4f9a
Federico Henzenn <fede.henzenn@gmail.com>
2022-04-11 18:41:17 -0300

    Table models
    Tables models create

--- models.py
+++ models.py
@@ -1,25 +1,35 @@
-from datetime import datetime
-from pydantic import BaseModel
+from sqlite3 import Date
+from xmlrpc.client import DateTime
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
-class Vacancy(BaseModel):
-    PositionName: str
-    CompanyId: str
-    Salary : int
-    MaxExperience: int
-    VacancyId: str
-    VacancyLink: str
-    MinExperience: int
-    Skills: str
+from .database import Base
 
-class Company(BaseModel):
-    Name: str
-    Link: str
-    City: str
-    DateAdded: datetime
-    ContactFirstName: str
-    ContactLastName: str
-    ContactPhoneNumber: str
-    ContactEmail: str
-    CompanyId: str
-    Country: str
+class Vacancy(Base):
+    __tablename__ = "vacancies"
+
+    vacancy_id = Column(Integer, primary_key=True, index=True)
+    position_name = Column(String)  
+    salary = Column(Integer)
+    max_experience = Column(Integer)
+    vacancy_link = Column(String)
+    min_experience = Column(Integer)
+    skills = Column(String)
+    company_id = Column(Integer, ForeignKey('companies.company_id'))
+
+class Company(Base):
+    __tablename__ = "companies"
     
+    company_id = Column(Integer, primary_key=True, index=True)
+    name = Column(Integer)
+    link = Column(Integer)
+    city = Column(Integer)
+    date_added = Column(DateTime) 
+    contact_first_name = Column(Integer)
+    contact_last_name = Column(Integer)
+    contact_phone_number = Column(Integer)
+    contact_email = Column(Integer)
+    country = Column(Integer)
+    vacancies = relationship("Vacancy")
+    
+
